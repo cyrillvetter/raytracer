01c039c11896d624cd24088379f2eaf829ca88f8
Cyrill Vetter <cyrill.vetter@gmail.com>
2025-03-07 21:35:15 +0100

    render bands in parallel

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod camera;
 use std::time::Instant;
 use minifb::{Window, WindowOptions, Key, KeyRepeat};
 use rand::Rng;
+use rayon::prelude::*;
 
 use crate::image::Image;
 use crate::vec3::Vec3;
@@ -22,7 +23,7 @@ const WINDOW_HEIGHT: u32 = 1080;
 const IMAGE_WIDTH: u32 = 1920;
 const IMAGE_HEIGHT: u32 = 1080;
 
-const LIGHT_ORIGIN: Vec3 = Vec3::new(-10.0, 7.0, 12.0);
+const LIGHT_ORIGIN: Vec3 = Vec3::new(-10.0, 12.0, 25.0);
 
 static OUT_PATH: &str = "out/image.png";
 
@@ -61,49 +62,58 @@ fn show_image(image: &Image) {
 }
 
 fn render_spheres() -> Image {
-    const OBJECTS_AMOUNT: usize = 1000;
+    const OBJECTS_AMOUNT: usize = 10000;
     let mut rng = rand::rng();
-    let mut objects: Vec<Box<dyn Hittable>> = Vec::with_capacity(OBJECTS_AMOUNT);
+    let mut objects: Vec<Sphere> = Vec::with_capacity(OBJECTS_AMOUNT);
 
     for _ in 0..OBJECTS_AMOUNT {
         let x = rng.random_range(-0.9..0.9);
-        let y = rng.random_range(-0.55..0.55);
-        let z = rng.random_range(1.0..10.0);
-        let radius = rng.random_range(0.02..0.07);
+        let y = rng.random_range(-0.50..0.50);
+        let z = rng.random_range(1.0..20.0);
+        let radius = rng.random_range(0.0075..0.02);
 
         let r = rng.random();
         let g = rng.random();
         let b = rng.random();
 
-        objects.push(Box::new(Sphere::new(Vec3::new(x, y, z), radius, Color::rgb(r, g, b))));
+        objects.push(Sphere::new(Vec3::new(x, y, z), radius, Color::rgb(r, g, b)));
     }
 
     let camera = Camera::new(1.0);
 
-    let mut image = Image::blank(IMAGE_WIDTH, IMAGE_HEIGHT);
+    let mut pixels = vec![0; (IMAGE_WIDTH * IMAGE_HEIGHT) as usize];
+    let bands: Vec<(usize, &mut [u32])> = pixels.chunks_mut(IMAGE_WIDTH as usize).enumerate().collect();
+
+    bands
+        .into_par_iter()
+        .for_each(|(y, band)| {
+            render_line(band, y as u32, &camera, &objects);
+        });
+
+    Image::new(IMAGE_WIDTH as u32, IMAGE_HEIGHT as u32, pixels)
+}
+
+fn render_line(pixels: &mut [u32], y: u32, camera: &Camera, objects: &Vec<Sphere>) {
     let ray_dir = Vec3::new(0.0, 0.0, -1.0).normalize();
 
-    for x in 0..IMAGE_WIDTH {
-        for y in 0..IMAGE_HEIGHT {
-            let pixel = camera.in_world(x, y);
-            let ray = Ray::new(pixel, ray_dir);
+    for x in 0..pixels.len() {
 
-            let mut nearest_dist = f32::INFINITY;
-            let mut color = Color::BLACK;
+        let pixel = camera.in_world(x as u32, y);
+        let ray = Ray::new(pixel, ray_dir);
 
-            for object in objects.iter() {
-                match object.hit(&ray) {
-                    Some(dist) if dist < nearest_dist => {
-                        nearest_dist = dist;
-                        color = object.get_color(ray.at(dist));
-                    },
-                    _ => ()
-                };
-            }
+        let mut nearest_dist = f32::INFINITY;
+        let mut color = Color::BLACK;
 
-            image.set_pixel(x, y, color);
+        for object in objects.iter() {
+            match object.hit(&ray) {
+                Some(dist) if dist < nearest_dist => {
+                    nearest_dist = dist;
+                    color = object.get_color(ray.at(dist));
+                },
+                _ => ()
+            };
         }
-    }
 
-    image
+        pixels[x] = ((color.r * 255.0) as u32) << 16 | ((color.g * 255.0) as u32) << 8 | ((color.b * 255.0) as u32);
+    }
 }
