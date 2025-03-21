d3783b3f1d2c8dcb1fbc2ca9bb3c0df904e2a7ca
Cyrill Vetter <cyrill.vetter@gmail.com>
2025-03-21 17:01:48 +0100

    add another broken implementation

--- src/hittable/triangle.rs
+++ src/hittable/triangle.rs
@@ -21,20 +21,38 @@ impl Triangle {
 impl Hittable for Triangle {
     // Barycentric coordinates intersection.
     fn hit(&self, ray: &Ray) -> Option<HitRecord> {
-        let t = ((self.v1 - ray.origin).dot(self.normal)) / ray.direction.dot(self.normal);
-        let p = ray.at(t);
+        let denominator = ray.direction.dot(self.normal);
+        if denominator > -f32::EPSILON && denominator < f32::EPSILON {
+            return None;
+        }
 
-        let calc_area = |a: Vec3, b: Vec3| {
-            0.5 * a.cross(b).length()
-        };
+        let lambda = (self.v1 - ray.origin).dot(self.normal) / denominator;
+        if lambda <= 0.0 {
+            return None;
+        }
 
-        let triangle_area = calc_area(self.v2 - self.v1, self.v1 - self.v3);
-        let t1 = calc_area(self.v2 - p, self.v2 - self.v1) / triangle_area;
-        let t2 = calc_area(self.v3 - p, self.v3 - self.v2) / triangle_area;
-        let t3 = calc_area(self.v1 - p, self.v1 - self.v3) / triangle_area;
+        let q = ray.at(lambda);
 
-        if t1 >= 0.0 && t2 >= 0.0 && t3 >= 0.0 && (t1 + t2 + t3) <= 1.0 {
-            Some(HitRecord::new(t, p, self.normal))
+        let v0 = self.v2 - self.v1;
+        let v1 = self.v3 - self.v1;
+        let v2 = q - self.v1;
+
+        let d00 = v0.dot(v0);
+        let d01 = v0.dot(v1);
+        let d11 = v1.dot(v1);
+
+        let discriminant = d00 * d11 - d01 * d01;
+        if discriminant > -f32::EPSILON && discriminant < f32::EPSILON {
+            return None;
+        }
+
+        let d20 = v2.dot(v0);
+        let d21 = v2.dot(v1);
+
+        let mu = (d11 * d20 - d01 * d21) / discriminant;
+        let tau = (d00 * d21 - d01 * d20) / discriminant;
+        if mu >= 0.0 && mu <= 1.0 && tau >= 0.0 && tau <= 1.0 && (mu + tau) <= 1.0 {
+            Some(HitRecord::new(lambda, q, self.normal))
         } else {
             None
         }
