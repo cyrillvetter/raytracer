1bec4fabfdb993b35ca0d012a29c40de4531dc91
Cyrill Vetter <cyrill.vetter@gmail.com>
2025-03-21 13:46:06 +0100

    implement next attempt

--- src/hittable/triangle.rs
+++ src/hittable/triangle.rs
@@ -23,17 +23,17 @@ impl Hittable for Triangle {
     fn hit(&self, ray: &Ray) -> Option<HitRecord> {
         let t = ((self.v1 - ray.origin).dot(self.normal)) / ray.direction.dot(self.normal);
         let p = ray.at(t);
-        let calc_area = |v1: Vec3, v2: Vec3| {
-            0.5 * (v1.cross(v2)).length()
+
+        let calc_area = |a: Vec3, b: Vec3| {
+            0.5 * a.cross(b).length()
         };
 
-        let triangle_area = calc_area(self.v1, self.v2);
+        let triangle_area = calc_area(self.v2 - self.v1, self.v1 - self.v3);
+        let t1 = calc_area(self.v2 - p, self.v2 - self.v1) / triangle_area;
+        let t2 = calc_area(self.v3 - p, self.v3 - self.v2) / triangle_area;
+        let t3 = calc_area(self.v1 - p, self.v1 - self.v3) / triangle_area;
 
-        let t1 = calc_area(p, self.v2) / triangle_area;
-        let t2 = calc_area(p, self.v3) / triangle_area;
-        let t3 = calc_area(p, self.v1) / triangle_area;
-
-        if t1 >= 0.0 && t2 >= 0.0 && t3 >= 0.0 && (t1 + t2 + t3) == 1.0 {
+        if t1 >= 0.0 && t2 >= 0.0 && t3 >= 0.0 && (t1 + t2 + t3) <= 1.0 {
             Some(HitRecord::new(t, p, self.normal))
         } else {
             None
